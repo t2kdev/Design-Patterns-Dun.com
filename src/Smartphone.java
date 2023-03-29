b639aae3d374c37930df59a5ec291f032c8ea73a
LucaEisinga <luca.eisinga@student.nhlstenden.com>
2023-03-29 11:48:32 +0200

    added products

---
+++ src/Smartphone.java
@@ -0,0 +1,5 @@
+public class Smartphone extends ProductBase {
+    public Smartphone(String name, double price) {
+        super(name, price);
+    }
+}
---
+++ src/Tshirt.java
@@ -0,0 +1,5 @@
+public class Tshirt extends ProductBase {
+    public Tshirt(String name, double price) {
+        super(name, price);
+    }
+}
