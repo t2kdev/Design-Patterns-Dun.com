6e07314310f442c28ed7c35ae2e68b83d5e5404e
LucaEisinga <luca.eisinga@student.nhlstenden.com>
2023-03-29 12:27:19 +0200

    Factories geimplementeerd

--- src/Factory.java
+++ src/Factory.java
@@ -1,3 +1,11 @@
 public class Factory implements ProductFactory {
 
+    @Override
+    public Product createProduct(String name, double price, String type) {
+        return switch (type) {
+            case "telefoon" -> new Smartphone(name, price);
+            case "kleding" -> new Tshirt(name, price);
+            default -> throw new IllegalArgumentException("Ongeldig product type: " + type);
+        };
+    }
 }
--- src/ProductFactory.java
+++ src/ProductFactory.java
@@ -1,3 +1,3 @@
 public interface ProductFactory {
-
+    Product createProduct(String name, double price, String type);
 }
